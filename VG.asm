06c16fb7c28c0dac520d9063665b6fb89b119fc9
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-25 20:22:36 +0100

    Update VG.asm

--- VG.asm
+++ VG.asm
@@ -1,4 +1,4 @@
-;********************************************************************************************************
+ ;********************************************************************************************************
 ; VG.asm: An assembler program containing serial transmission, where an LED 
 ;         connected to pin 8 (PORTB0) is pulled via pressing a push button connected 
 ;         to pin 13 (PORTB5). At startup, the following instructions should be printed in the terminal:
@@ -39,30 +39,47 @@
    RJMP main
 
 ;/********************************************************************************
-;* ISR_PCINT0: Avbrottsrutin för PCI-avbrott på I/O-port, som anropas vid
-;             nedtryckning och uppsläppning av tryckknapparna. Vid nedtryckning
-;             togglas Timer 1 eller timer 2. Om Timer 1 eller 2 stängs av släcks 
-;             respektive lysdiod.
+;* PCINT0_vect: Avbrottsvektor för PCI-avbrott på I/O-port B. Vid avbrott sker
+;*              programhopp till motsvarande avbrottsrutin ISR_PCINT0.
+;********************************************************************************/
+.ORG PCINT0_vect 
+   RJMP ISR_PCINT0 ; Hoppar till motsvarande avbrottsrutin ISR_PCINT0.
+
+;/********************************************************************************
+;* ISR_PCINT0: Avbrottsrutin för PCI-avbrott på I/O-port B, som äger rum vid
+;*             nedtryckning och uppsläppning av tryckknappen. Vid nedtryckning 
+;*             togglas lysdioden, annars görs ingenting.
 ;********************************************************************************/
 ISR_PCINT0:
-   CLR R24
-   STS PCICR, R24
-   STS TIMSK0, R16
-   ISR_PCINT_1:
-   IN R24, PINB
-   ANDI R24, (1 << BUTTON1)
-   BREQ ISR_PCINT0_end
-   CALL system_reset
+   IN R17, PINB             ; Läser insignaler från PINB, sparar en kopia i R17.
+   ANDI R17, (1 << BUTTON1) ; Multiplicerar bitvis med 0010 0000.
+   BREQ ISR_PCINT0_end      ; Om kvarvarande värde är lika med 0x00 görs ingenting.x
+   OUT PINB, R16            ; Annars togglas lysdioden (0000 0001 ligger kvar i R16).
 ISR_PCINT0_end:
-   RETI
+   RETI                     ; Avslutar avbrottet och återställer systemet.
 
+;/********************************************************************************
+;* setup: Initierar I/O-portar (lysdioden sätts till utport och den interna 
+;*        pullup-resistorn på tryckknappens pin aktiveras) samt aktiverar 
+;*        PCI-avbrott på tryckknappens pin. Denna subrutin placeras inline i
+;*        i main för att effektivisera programmet (vi slipper programhopp samt
+;*        återhopp).
+;********************************************************************************/
+setup:                    
+   LDI R16, (1 << LED1)    ; Läser in värdet 0000 0001 i CPU-register R16.
+   OUT DDRB, R16           ; Sätter lysdioden till utport.
+   LDI R17, (1 << BUTTON1) ; Läser in värdet 0010 0000 i CPU-register R17.
+   OUT PORTB, R17          ; Aktiverar den interna pullup-resistorn på tryckknappens pin.
+   SEI                     ; Aktiverar avbrott globalt.
+   STS PCICR, R16          ; Aktiverar PCI-avbrott på I/O-port B.
+   STS PCMSK0, R17         ; Aktiverar PCI-avbrott på tryckknappens pin 13 (PORTB5).
 
 ;********************************************************************************
 ; main: Initierar systemet vid start. Programmet hålls sedan igång så länge
 ;       matningsspänning tillförs.
-;********************************************************************************b
+;********************************************************************************
 main:
    RCALL setup
 main_loop:
    RJMP main_loop
-
+                      
